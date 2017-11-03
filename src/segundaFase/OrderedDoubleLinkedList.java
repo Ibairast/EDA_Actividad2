1968f7629f332c85dde0b094993097ad853c05b1
andrea98gar <andrea98.gar@gmail.com>
2017-11-03 19:36:20 +0100

    Corrección Ordenar

--- src/segundaFase/OrderedDoubleLinkedList.java
+++ src/segundaFase/OrderedDoubleLinkedList.java
@@ -1,9 +1,10 @@
 package segundaFase;
 
 
-public class OrderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements OrderedListADT<T> {
+public class OrderedDoubleLinkedList<T extends Comparable<T>> extends DoubleLinkedList<T> implements OrderedListADT<T> {
 
 
+<<<<<<< Updated upstream
     public void add(T elem) {
         Node<T> newNode = new Node<>(elem);
         boolean enc = false;
@@ -55,6 +56,64 @@ public class OrderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements O
 
         this.count++;
     }
+=======
+	public void add(T elem){
+		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+		Node<T> newNode = new Node<>(elem);
+		boolean enc = false;
+		if (first == null) {
+			newNode.prev = newNode;
+			newNode.next = newNode;
+			first = newNode;
+		}else{
+			Node<T> act = first;
+			int cont = 1;
+			while (!enc && cont <= this.count){
+				int x = (newNode.data).compareTo(act.data);
+				if (x == 1 && cont == this.count){ 
+					if(act == first){
+						newNode.next = act;
+						newNode.prev = act;
+						first.prev = newNode;
+						first.next = newNode;
+						enc = true;
+					}else{
+						newNode.prev = act;
+						newNode.next = act.next;
+						act.next.prev = newNode;
+						act.next = newNode;
+						enc = true;
+					}
+				}else if(x == -1){
+					if (act == first){
+						newNode.next = act;
+						newNode.prev = act.prev;
+						act.prev.next = newNode;
+						first = newNode;
+						act.prev = newNode;
+						enc = true;
+					}else{
+						newNode.next = act;
+						newNode.prev = act.prev;
+						act.prev.next = newNode;
+						act.prev = newNode;
+						enc = true;
+					}
+				}else if (x == 0){
+					newNode.next = act.next;
+					newNode.prev = act;
+					act.next.prev = newNode;
+					act.next = newNode;
+					enc = true;
+				}
+				act = act.next;
+				cont ++;
+			}
+		}
+
+		this.count++;
+	}
+>>>>>>> Stashed changes
 
 
 }
